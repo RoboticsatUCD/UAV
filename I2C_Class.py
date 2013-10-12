679a8b1e799d2d7ef8e6edcd9ffb5fda862b2eda
Evan Racah <evanracah@Evans-MacBook-Pro.local>
2013-10-11 17:42:30 -0700

    added I2C Class and registers module

---
+++ I2C_Class.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+from robovero.extras import Array, roboveroConfig
+from robovero.lpc17xx_i2c import I2C_M_SETUP_Type, I2C_MasterTransferData, \
+														I2C_TRANSFER_OPT_Type
+from robovero.lpc17xx_gpio import GPIO_ReadValue
+from robovero.LPC17xx import LPC_I2C0
+from robovero.lpc_types import Status
+import time
+import math
+
+
+
+class I2CDevice(object):
+	def __init__(self, address):
+		self.config = I2C_M_SETUP_Type()
+		self.tx_data = Array(2, 1)
+		self.rx_data = Array(1, 1)
+		self.config.sl_addr7bit = address
+		self.config.tx_data = self.tx_data.ptr
+		self.config.retransmissions_max = 3
+
+	def readReg(self, register):
+		self.tx_data[0] = register
+		self.config.tx_length = 1
+		self.config.rx_data = self.rx_data.ptr
+		self.config.rx_length = 1	
+		ret = I2C_MasterTransferData(LPC_I2C0, self.config.ptr,
+																	I2C_TRANSFER_OPT_Type.I2C_TRANSFER_POLLING)
+		if ret == Status.ERROR:
+			exit("I2C Read Error")		
+		return self.rx_data[0]
+		
+	def writeReg(self, register, value):
+		self.tx_data[0] = register
+		self.tx_data[1] = value
+		self.config.tx_length = 2
+		self.config.rx_data = 0
+		self.config.rx_length = 0
+		ret = I2C_MasterTransferData(LPC_I2C0, self.config.ptr,
+																	I2C_TRANSFER_OPT_Type.I2C_TRANSFER_POLLING)
+		if ret == Status.ERROR:
+			exit("I2C Write Error")
+		if self.readReg(register) != value:
+			exit("I2C Verification Error")
+		return None
+
+		def twosComplement(low_byte, high_byte):
+	"""Unpack 16-bit twos complement representation of the result.
+	"""
+	return (((low_byte + (high_byte << 8)) + 2**15) % 2**16 - 2**15)
---
+++ registers.py
@@ -0,0 +1,30 @@
+accel_ctrl_reg4 = 0x23
+accel_ctrl_reg1 = 0x20
+accel_x_low = 0x28
+accel_x_high = 0x29
+accel_y_low = 0x2A
+accel_y_high = 0x2B
+accel_z_low = 0x2C
+accel_z_high = 0x2D
+
+compass_mr_reg = 0x02
+compass_x_high = 0x03
+compass_x_low = 0x04
+compass_y_high = 0x05
+compass_y_low = 0x06
+compass_z_high = 0x07
+compass_z_low = 0x08
+
+gyro_ctrl_reg1 = 0x20
+gyro_ctrl_reg2 = 0x21
+gyro_ctrl_reg3 = 0x22
+gyro_ctrl_reg4 = 0x23
+gyro_ctrl_reg5 = 0x24
+gyro_status_reg = 0x27
+gyro_x_low = 0x28
+gyro_x_high = 0x29
+gyro_y_low = 0x2A
+gyro_y_high = 0x2B
+gyro_z_low = 0x2C
+gyro_z_high = 0x2D
+gyro_fifo_ctrl_reg = 0x2E
