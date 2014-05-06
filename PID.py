4636557db9ee182026e633ecd9741221964bf647
Evan Racah <ejracah@gmail.com>
2014-05-06 11:44:02 -0700

    Fixed a bug in PID

--- PID.py
+++ PID.py
@@ -1,45 +1,39 @@
-'''
-Created on Aug 20, 2013
+__author__ = 'Evan Racah'
 
-@author: ejracah
-'''
+
 import time
 
+
 class PIDControl(object):
-    def __init__(self,setpoint,coefficients):
+    def __init__(self, setpoint, coefficients): 
         self.setpoint = setpoint
         self.lastTime = time.time()
         self.kP, self.kI, self.kD = coefficients
         self.errorTotal = 0.0
         self.previousError = 0.0
-        self.max_error = 180
-        self.min_error = -180
+        #self.max_error = 180
+        #self.min_error = -180
 
     def update(self, measurement):
-
-        #get new time interval
+        #update time stuff
         self.now = time.time()
         self.timeInterval = self.now - self.lastTime
-        
-        #calculate error terms
-        self.error = max(min(self.setpoint - measurement, self.max_error),self.min_error)
+
+        #calculate the three error terms
+        self.error = self.setpoint - measurement
         self.errorTotal += self.error * self.timeInterval
         self.dError = (self.error - self.previousError) / self.timeInterval
-        
-        
-      
-        #calculate the three PID terms
-        self.P = self.kP * self.error
-        self.I = self.kI * self.errorTotal * self.timeInterval
-        self.D = self.kD * (self.dError / self.timeInterval)
 
-        #set new previous termsp
+        #multiply by coefficents
+        P = self.kP * self.error
+        I = self.kI * self.errorTotal
+        D = self.kD * self.dError
+        
+        #update time and error stuff
         self.lastTime = self.now
         self.previousError = self.error
-        
-      
-        
-        return self.P + self.I + self.D
 
-    def set_set_point(self,setpoint):
+        return P + I + D
+
+    def set_set_point(self, setpoint):
         self.setpoint = setpoint
